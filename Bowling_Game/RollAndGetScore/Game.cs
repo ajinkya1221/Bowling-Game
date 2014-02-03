2c85a4bea77e912c7e08ff420cc0bf072ebc03f0
unknown <abhagwat@Lab6.pune.tavisca.com>
2014-02-03 08:47:36 +0530

    Bowling game working fine

---
+++ Bowling_Game/RollAndGetScore/Game.cs
@@ -0,0 +1,76 @@
+﻿namespace RollAndGetScore
+{
+    interface IGame
+    {
+        void Roll(int currentScore);
+        int GetScore();
+    }
+
+    public class Game:IGame
+    {
+        private readonly int[] _rollArray = new int[21];
+        
+        private int _currentRoll;
+        private int _totalScore;
+        private int _rollCount;
+
+        public void Roll(int currentScore)
+        {
+            _rollArray[_currentRoll] = currentScore;
+            _currentRoll++;
+        }
+
+        public int GetScore()
+        {            
+            for (int frameCount = 0; frameCount < 10; frameCount++)
+            {                    
+                if (IsStrike())
+                {
+                    _totalScore = _totalScore + GetFrameScore(_rollCount) + GetBonus(_rollCount);
+                    _rollCount++;
+                }                                    
+                else if (IsSpare())
+                {
+                    _totalScore = _totalScore + GetFrameScore(_rollCount) + GetBonus(_rollCount);
+                    _rollCount = _rollCount + 2;
+                }                    
+                else
+                {
+                    _totalScore = _totalScore + GetFrameScore(_rollCount) + GetBonus(_rollCount);
+                    _rollCount = _rollCount + 2;
+                }
+            }
+                return _totalScore;
+        }
+
+        private bool IsStrike()
+        {
+            if (_rollArray[_rollCount] == 10)
+                return true;
+            return false;
+        }
+
+        private bool IsSpare()
+        {
+            if (_rollArray[_rollCount] + _rollArray[_rollCount + 1] == 10)            
+                return true;            
+            return false;
+        }
+
+        private int GetFrameScore(int rollCount)
+        {
+            if (IsSpare() || IsStrike())            
+                return 10;            
+            return _rollArray[rollCount] + _rollArray[rollCount + 1];
+        }
+
+        private int GetBonus(int rollCount)
+        {
+            if (IsStrike())            
+                return _rollArray[rollCount + 1] + _rollArray[rollCount + 2];            
+            if(IsSpare())            
+                return _rollArray[rollCount + 2];            
+            return 0;
+        }
+    }
+}
---
+++ Bowling_Game/Test.BowlingGame/Testcases.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RollAndGetScore;
+
+namespace Test.BowlingGame
+{
+    [TestFixture]
+    public class Testcases
+    {
+        private Game _game;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _game = new Game();
+        }
+
+        public void TraverseRoll(int frame, int pins)
+        {
+            for (int i = 0; i < frame; i++)
+                _game.Roll(pins);
+        }
+
+        [Test]
+        public void AllGutterRollScenario()
+        {
+            TraverseRoll(21, 0);
+            Assert.AreEqual(0, _game.GetScore());
+        }
+
+        [Test]
+        public void SimpleRollScenario()
+        {
+            TraverseRoll(20, 1);
+            Assert.AreEqual(20, _game.GetScore());
+        }
+
+        [Test]
+        public void AllStrikesScenario()
+        {
+            TraverseRoll(12, 10);
+            Assert.AreEqual(300, _game.GetScore());
+        }
+
+        [Test]
+        public void AllSpareScenario()
+        {
+            TraverseRoll(21, 5);
+            Assert.AreEqual(150, _game.GetScore());
+        }
+    }
+}
